fc30dd86427091858f2f95c04fb3c036aaba3cb4
Jack Kleeman <jackkleeman@gmail.com>
2023-12-07 13:16:30 +0000

    Add restate.rb

---
+++ release/release.sh
@@ -0,0 +1,12 @@
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+version=$(curl --silent --location --fail https://api.github.com/repos/restatedev/restate/releases/latest | grep '^  "name"' | awk '{print $2}' | tr -d '"v,')
+darwin_x86=$(curl --silent --location --fail "https://github.com/restatedev/restate/releases/download/v${version}/restate.x86_64-apple-darwin.tar.gz" | sha256sum | awk '{print $1}')
+darwin_arm64=$(curl --silent --location --fail "https://github.com/restatedev/restate/releases/download/v${version}/restate.aarch64-apple-darwin.tar.gz" | sha256sum | awk '{print $1}')
+linux_x86=$(curl --silent --location --fail "https://github.com/restatedev/restate/releases/download/v${version}/restate.x86_64-unknown-linux-gnu.tar.gz" | sha256sum | awk '{print $1}')
+linux_arm64=$(curl --silent --location --fail "https://github.com/restatedev/restate/releases/download/v${version}/restate.aarch64-unknown-linux-gnu.tar.gz" | sha256sum  | awk '{print $1}')
+export version darwin_x86 darwin_arm64 linux_x86 linux_arm64
+
+envsubst < "${SCRIPT_DIR}/restate.rb.tmpl" > "${SCRIPT_DIR}/../formula/restate.rb"
