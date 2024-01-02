1185d1e7e184905d9ec7d66e7f751623aa82aef0
Jack Kleeman <jackkleeman@gmail.com>
2024-01-02 15:15:21 +0000

    Add update.yml

--- release/release.sh
+++ release/release.sh
@@ -9,4 +9,12 @@ linux_x86=$(curl --silent --location --fail "https://github.com/restatedev/resta
 linux_arm64=$(curl --silent --location --fail "https://github.com/restatedev/restate/releases/download/v${version}/restate.aarch64-unknown-linux-gnu.tar.gz" | sha256sum  | awk '{print $1}')
 export version darwin_x86 darwin_arm64 linux_x86 linux_arm64
 
-envsubst < "${SCRIPT_DIR}/restate.rb.tmpl" > "${SCRIPT_DIR}/../formula/restate.rb"
+envsubst < "${SCRIPT_DIR}/restate.rb.tmpl" > "${SCRIPT_DIR}/../Formula/restate.rb"
+
+git -C "${SCRIPT_DIR}/.." reset
+git -C "${SCRIPT_DIR}/.." add "Formula/restate.rb"
+if git diff --cached --exit-code; then
+  echo "Nothing to commit"
+else
+  git -C "${SCRIPT_DIR}/.." commit -m "Update to ${version}"
+fi
