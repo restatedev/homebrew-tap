94cae7180777281d6280cac618a670b756f787b1
Till Rohrmann <till@restate.dev>
2025-04-16 12:08:07 +0200

    Revert "restate 1.2.3"
    This reverts commit 4512b1c920660515ea323982b6792f9123310ae1.

--- Formula/restate.rb
+++ Formula/restate.rb
@@ -1,25 +1,25 @@
 class Restate < Formula
   desc "Restate CLI"
   homepage "https://github.com/restatedev/restate"
-  version "1.2.3"
+  version "1.3.1"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://restate.gateway.scarf.sh/v1.2.3/restate-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "353c6e863371ce8045707593a41c29fe82179c1d7788bb69cc7d01e167a00220"
+      url "https://restate.gateway.scarf.sh/v1.3.1/restate-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "172ba59ff030e23190504732f8c738406901e998b74f630f293a7440427a4e8f"
     end
     if Hardware::CPU.intel?
-      url "https://restate.gateway.scarf.sh/v1.2.3/restate-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "00e9a82880400a8b35561af1b516aa08888ba7ce8797b603b791febf9b707103"
+      url "https://restate.gateway.scarf.sh/v1.3.1/restate-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "f41126ec945862d23b99d9a0a7c4b8963f28f5c95ace0e4b9a36efb5aae864a5"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://restate.gateway.scarf.sh/v1.2.3/restate-cli-aarch64-unknown-linux-musl.tar.xz"
-      sha256 "db90f65740cda8dc4b5d84057ee2d12e754cee6353534a7303542b43b6b38a64"
+      url "https://restate.gateway.scarf.sh/v1.3.1/restate-cli-aarch64-unknown-linux-musl.tar.xz"
+      sha256 "35df2af5e15cf510f8638821773267a9ef40c76fa52ff7a733da7c9449497928"
     end
     if Hardware::CPU.intel?
-      url "https://restate.gateway.scarf.sh/v1.2.3/restate-cli-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "0359b0e58fe65dad1fb9d12595df0706fee03b7bd9b47dc0dd513cf850a769f0"
+      url "https://restate.gateway.scarf.sh/v1.3.1/restate-cli-x86_64-unknown-linux-musl.tar.xz"
+      sha256 "3631d9f3238b7067a4778cf0a3059f66dd2e27200834a717d64f4a2ac56ddcca"
     end
   end
   license "BUSL-1.1"
